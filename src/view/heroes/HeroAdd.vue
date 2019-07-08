aa593da47fad4b7f95e6aa6c1250ed994fd841ff
ql-github <905972934@qq.com>
2019-07-08 20:05:25 +0800

    添加功能完成

--- src/view/heroes/HeroAdd.vue
+++ src/view/heroes/HeroAdd.vue
@@ -4,7 +4,7 @@
     <form>
       <div class="form-group">
         <label for="name">姓名</label>
-        <input type="text" class="form-control" id="name" placeholder="Name" />
+        <input type="text" class="form-control" id="name" placeholder="Name" v-model="fromData.name"/>
       </div>
       <div class="form-group">
         <label for="gender">性别</label>
@@ -12,16 +12,40 @@
           type="text"
           class="form-control"
           id="gender"
-          placeholder="Gender"
+          placeholder="Gender" 
+          v-model="fromData.gender"
         />
       </div>
-      <button type="submit" class="btn btn-success">添加</button>
+      <button type="button" class="btn btn-success" @click.prevent="add">添加</button>
     </form>
   </div>
 </template>
 
 <script>
-export default {};
+import axios from 'axios';
+
+export default {
+    data() {
+        return {
+            // 绑定文本框
+            fromData:{
+                name:'',
+                gender:''
+            }
+        }
+    },
+    methods: {
+        add(){
+          axios.post("http://localhost:3000/heroes",this.fromData) 
+          .then((res)=>{
+              if(res.status==201){
+              // 添加成功跳转回列表页面
+                this.$router.push({path: '/heroes'})
+              }
+          })
+        }
+    },
+};
 </script>
 
 <style>
